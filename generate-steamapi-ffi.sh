b00c0a4695747875b40215abab87bab3b07d2e56
Hunter Perrin <hperrin@gmail.com>
2022-08-18 13:09:03 -0700

    Trying to get clangffi working.

--- generate-steamapi-ffi.sh
+++ generate-steamapi-ffi.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-npx clangffi --lib-path /usr/lib/libclang.so -L cpp -i $HOME/steamworks_sdk_*/sdk/public/steam/steam_api.h --include-directory $HOME/steamworks_sdk_*/sdk/public/steam/ --include "*" -o ./server/steam_api.ts
+npx clangffi --lib-path ~/llvm-project-13.0.0.src/build/lib/libclang -L cpp -i ./steam/steam_api.h --include-directory ./steam/ --include "*" -o ./server/steam_api.ts
